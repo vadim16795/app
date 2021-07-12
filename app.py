3cbac76e845290666db9d7b341df1888bede6766
vadim16795 <explosivedust@yandex.ru>
2021-07-12 20:17:07 +0300

    change app.py add html add /ping route

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template,jsonify
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, Everyone!"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ping')
+def ping():
+    resp = jsonify(success=True)
+    resp.status_code = 200
+    return resp
