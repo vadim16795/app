84199036d8079c926c1720076c5b8ab8e3096613
vadim16795 <explosivedust@yandex.ru>
2021-07-12 22:11:11 +0300

    try connect to database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-
+import psycopg2
 app = Flask(__name__)
 
 
@@ -13,3 +13,15 @@ def ping():
     resp = jsonify(success=True)
     resp.status_code = 200
     return resp
+
+@app.route('/select')
+def select():
+    connection = psycopg2.connect(dbname='postgres', user='varkhipov@varkhipovazurepgsqlsrv', password='H@Sh1CoR3!', host='varkhipovazurepgsqlsrv.postgres.database.azure.com')
+    cursor = connection.cursor()
+    select_query = """ SELECT * FROM characters"""
+    cursor.execute(select_query)
+    records = cursor.fetchall()
+    if records is not None:
+        return "OK"
+
+
