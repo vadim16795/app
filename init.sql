e7e1f3a9b297f18bd26cafd919e44608d805da82
vadim16795 <explosivedust@yandex.ru>
2021-08-02 16:44:10 +0300

    update init.sql

--- init.sql
+++ init.sql
@@ -1,2 +1,4 @@
 create table characters(id int, name varchar, gender varchar, homeworld varchar);
 insert into characters (name, gender, homeworld) values ('Anakin Skywalker', 'male', 'Tatooine');
+create table planets(id int, name varchar, climate varchar, residents varchar[]);
+insert into planets (name, gravity,climate, residents) values ('Tatooine', '1 standart', 'arid','Luke Skywalker');
