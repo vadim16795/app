12b7460c84dd2757ee2fb8156eccd267763e0384
vadim16795 <explosivedust@yandex.ru>
2021-08-02 16:31:42 +0300

    update init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,2 @@
-create table sometable(id int);
+create table characters(id int, name varchar, gender varchar, homeworld varchar);
+insert into characters (name, gender, homeworld) values ('Anakin Skywalker', 'male', 'Tatooine');
