9f93efcbb58170b76f525f2dcecadd6cbd87d5e9
vadim16795 <explosivedust@yandex.ru>
2021-08-02 19:18:43 +0300

    edit test_app.py

--- test_app.py
+++ test_app.py
@@ -2,10 +2,10 @@ from app import app
 #def test_smth():
 #    1 + 1 == 2
 
-def test_home():
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
+#def test_home():
+#    client = app.test_client()
+#    response = client.get('/')
+#    assert response.status_code == 200
 
 def test_characters():
     client = app.test_client()
