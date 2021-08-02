856135f9e87f0a7e996d93765407a3125fda9fe1
vadim16795 <explosivedust@yandex.ru>
2021-08-02 13:52:43 +0300

    update test_app.py add /characters test

--- test_app.py
+++ test_app.py
@@ -1,8 +1,13 @@
 from app import app
-def test_smth():
-    1 + 1 == 2
+#def test_smth():
+#    1 + 1 == 2
 
-def test_more():
+def test_home():
     client = app.test_client()
     response = client.get('/')
+    assert response.status_code == 200
+
+def test_characters():
+    client = app.test_client()
+    response = client.get('/characters')
     assert response.status_code == 200
