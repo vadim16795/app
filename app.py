86158f77e96789502eb19713d18e812d3631acab
vadim16795 <explosivedust@yandex.ru>
2021-08-05 18:51:45 +0300

    add enviroment shit

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import db
 import os
 
 app = Flask(__name__)
-app.config.from_object('docker_settings')
-#app.config.from_envvar('DOCKERAPP_CONFIG', silent=True)
+#app.config.from_object('docker_settings')
+app.config.from_envvar('DOCKERAPP_CONFIG', silent=True)
 
 
 @app.route('/')
