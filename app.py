2f30bf2db1afb92e515f6fad868069bd65c7e5a9
vadim16795 <explosivedust@yandex.ru>
2021-09-07 19:10:11 +0300

    add metrics to app

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, Response
 import psycopg2
 import truncate_func
 import urllib.request
+import prometheus_client
+from prometheus_client.core import CollectorRegistry
+from prometheus_client import Summary,Counter,Histogram,Gauge
 import json
 import time
 import os
 from joblib import Parallel, delayed
 
 app = Flask(__name__)
-
+_INF = float("inf")
+graphs = {}
+graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
+graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds', buckets=(1,2,5,6,10,_INF))
 @app.route('/')
 def index():
+    start = time.time()
+    graphs['c'].inc()
     return render_template('index.html')
 
+@app.route('/metrics')
+def requests_count():
+    res = []
+    for k,v in graphs.items():
+        res.append(prometheus_client.generate_latest(v))
+    return Response(res,mimetype="text/plain")
 
 @app.route('/planets')
 def planets():
